fc7c1181bf12bacaf3815d623e23d7edcc012023
Jin <jinyung7428@gmail.com>
2024-05-21 13:37:22 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/webtoonix/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.webtoonix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:webtoonix/screens/home_screen.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      scrollBehavior: const MaterialScrollBehavior().copyWith(
+        dragDevices: {
+          PointerDeviceKind.mouse,
+          PointerDeviceKind.touch,
+          PointerDeviceKind.stylus,
+          PointerDeviceKind.unknown
+        },
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
